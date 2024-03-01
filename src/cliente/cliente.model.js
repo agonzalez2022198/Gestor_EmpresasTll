75489c3cc5454c5f414bfbab1377c163f0e44f23
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-29 23:41:53 -0600

    Clientes

---
+++ src/cliente/cliente.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const ClienteSchema = mongoose.model({
+
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+
+    apellido: {
+        type: String,
+        required: [true, 'El apellido es obligatorio']
+    },
+
+    correo: {
+        type: String,
+        required: [true, 'Es necesario el Email']
+    },
+
+    number: {
+        type: String,
+        required: [true, 'El número es obligatorio|']
+    },
+
+    registro: {
+        type: Date,
+        default: Date.now,
+        required: [true, 'El registro es automático, parce.']
+    },
+
+    estado: {
+        type: Boolean,
+        default: true
+    }
+
+});
+
+
+export default mongoose.model('Cliente', ClienteSchema);
